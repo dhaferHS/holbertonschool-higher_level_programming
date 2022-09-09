20ab48a5551f60d9630e1134dcfa2df4168732dd
dhaferHS <dhafer.darkprod@gmail.com>
2022-09-09 20:14:16 +0100

    100-print_tebahpla.py

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for c in range(122, 96):
+for c in range(96, 122):
     if c % 2 == 0:
         print("{}".format(chr(c)), end="")
     else:
         print("{}".format(chr(c - 32)), end="")
-        
+ 
