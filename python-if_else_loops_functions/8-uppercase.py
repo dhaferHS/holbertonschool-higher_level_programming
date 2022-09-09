cd7d07429aeff957fea8283efcb9526f6b8781f1
dhaferHS <dhafer.darkprod@gmail.com>
2022-09-09 16:28:31 +0100

    8-uppercase.py

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        if ord(i) > 96 and ord(i) < 123:
+            c = chr(ord(i) - 32)
+        else:
+            c = i
+        print("{}".format(c), end='')
+    print()
