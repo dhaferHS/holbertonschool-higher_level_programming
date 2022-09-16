da8aff02f15dabb83045904c4d26303543378cdd
dhaferHS <dhafer.darkprod@gmail.com>
2022-09-16 22:55:30 +0100

    3-common_elements.py

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
+    s = ()
     if len(set_1.intersection(set_2)) > 0:
         return (set_1.intersection(set_2))
+    else:
+        return (s)
