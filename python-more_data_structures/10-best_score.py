c86d43a1449f11bca0a311e65893248682cc10c2
dhaferHS <dhafer.darkprod@gmail.com>
2022-09-17 03:48:48 +0100

    10-best_score.py

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    Keymax = max(a_dictionary, key= lambda x: a_dictionary[x])
-    return(Keymax)
+    if len(a_dictionary) > 0:
+        Keymax = max(a_dictionary, key=lambda x: a_dictionary[x])
+    return (Keymax)
+    else:
+        return (None)
