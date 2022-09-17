ae68c49eff4f25e222ca4144843a4066854d5765
dhaferHS <dhafer.darkprod@gmail.com>
2022-09-17 18:35:06 +0100

    102-complex_delete.py

---
+++ python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    if a_dictionary is  None:
+        return (a_dictionary)
+    for key in list(a_dictionary.keys()):
+        if a_dictionary.get(key) == value:
+            del a_dictionary[key]
+   
+    return (a_dictionary)
