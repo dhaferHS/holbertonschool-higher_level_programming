f663a674ad4115169740faee26ddcb5a33b562ea
dhaferHS <dhafer.darkprod@gmail.com>
2022-09-17 04:27:13 +0100

    11-multiply_list_map.py

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    temp = []
-
-    temp = [list(map(lambda x: x * number, my_list))]
-    return (temp)
+    return (temp = [list(map(lambda x: x * number, my_list))])
