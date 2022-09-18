3da1b95bac3195ad9994db1aa43d34f6d2576faa
dhaferHS <dhafer.darkprod@gmail.com>
2022-09-18 16:07:09 +0100

    0-safe_print_list.py

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        c = 0
+        for i in range(0, x):
+            print("{}".forat(my_list[i]), end = "")
+            c = c + 1
+    except:
+        pass
+    finally:
+       print()
+       return (c)
