ee88687ff8e8a8c6a38299af9d0bcc6639f735d8
dhaferHS <dhafer.darkprod@gmail.com>
2022-09-18 17:03:13 +0100

    0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,10 +3,10 @@ def safe_print_list(my_list=[], x=0):
     try:
         c = 0
         for i in range(0, x):
-            print("{}".format(my_list[i]), end = "")
+            print("{}".format(my_list[i]), end="")
             c = c + 1
     except:
         pass
     finally:
-       print()
-       return (c)
+        print()
+        return (c)
