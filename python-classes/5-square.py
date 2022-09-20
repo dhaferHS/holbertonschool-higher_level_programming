9906138b5d35d0c15634b3cf39ef2182286c4d3b
dhaferHS <dhafer.darkprod@gmail.com>
2022-09-20 17:00:44 +0100

    5-main.py

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -25,11 +25,11 @@ class Square:
 
     def area(self):
         return self.__size * self.__size
-    
+
     def my_print(self):
         if self.__size == 0:
             print()
         for i in range(self.__size):
             for j in range(self.__size):
-                    print("#", end="")
+                print("#", end="")
             print()
