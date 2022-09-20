bee68432fb0a118d7534dccba522c976b1c9cce7
dhaferHS <dhafer.darkprod@gmail.com>
2022-09-20 16:50:10 +0100

    4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -9,10 +9,11 @@ class Square:
 
     def __init__(self, size=0):
         self.__size = size
+
     @property
     def size(self):
         return self.__size
-    
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
