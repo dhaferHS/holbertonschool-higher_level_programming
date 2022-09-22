bfa258d69b8203c68d83b678c878d9d73d9986ce
dhaferHS <dhafer.darkprod@gmail.com>
2022-09-22 11:37:20 +0100

    0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """Funtion adds 2 integers"""
 
+
 def add_integer(a, b=98):
     """ Function that adds two strings.
     Args: a (int) and  b (int)
     Returns: The sum
     """
-    if (type(a) != int and type(a) != float) or a != a:
+    if (not isinstance(a, int) and not isinstance(a, float)) or a != a:
         raise TypeError("a must be an integer")
-    if type(b) != int and type(b) != float or b != b:
+    if not isinstance(b, int) and not isinstance(b, float) or b != b:
         raise TypeError("b must be an integer")
     return int(a) + int(b)
