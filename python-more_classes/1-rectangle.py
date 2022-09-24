d33ff2adb9ea85886b0f4fe2a4117467984f6dbc
dhaferHS <dhafer.darkprod@gmail.com>
2022-09-24 21:09:20 +0100

    1-rectangle.py

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+class empty rectangle
+"""
+
+
+class Rectangle:
+    """
+class empty Rectangle
+    """
+
+    def __init__(self, width=0, height=0):
+        self.__height = height
+        self.__width = width
+
+
+@property
+def width(self):
+    return self.__width
+
+
+@width.setter
+def widht(self, value):
+    if not isinstance(value, int):
+        raise TypeError("width must be an integer")
+
+    if value < 0:
+        raise ValueError("width must be >= 0")
+    else:
+        self.__widht = value
+
+
+@property
+def height(self):
+    return self.__height
+
+
+@height.setter
+def height(self, value):
+    if not isinstance(value, int):
+        raise TypeError("must be an integer")
+
+    if value < 0:
+        raise ValueError("height must be >= 0")
+    else:
+        self.__height = value
