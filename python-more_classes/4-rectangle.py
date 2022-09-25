962e725225b730b99fd12749b80f5d7e04329e90
dhaferHS <dhafer.darkprod@gmail.com>
2022-09-25 19:40:13 +0100

    4-rectangle.py

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -56,5 +56,6 @@ class Rectangle:
             if i != self.__height - 1:
                 add = add + ("\n")
         return (add)
+
     def __repr__(self):
         return ("Rectangle({}, {})".format(self.width, self.height))
