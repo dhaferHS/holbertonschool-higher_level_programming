03fa689c4ccf46263344ecf7896b4335fe69188b
dhaferHS <dhafer.darkprod@gmail.com>
2022-09-25 20:08:59 +0100

    6-rectangle.py

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -7,11 +7,11 @@ class
 class Rectangle:
     """class Rectangle"""
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         Rectangle.number_of_instances += 1
         self.width = width
         self.height = height
-        
 
     @property
     def width(self):
