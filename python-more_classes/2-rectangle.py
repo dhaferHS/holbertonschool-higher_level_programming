2ccd272f809549236dd59ef498fa2872bacad4a5
dhaferHS <dhafer.darkprod@gmail.com>
2022-09-25 18:45:59 +0100

    2-rectangle.py

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -34,10 +34,10 @@ class Rectangle:
         if value < 0:
             raise ValueError('height must be >= 0')
         self.__height = value
-        
+
     def area(self):
         return self.__width * self.__height
-    
+
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
             return 0
