2554c365944aeda1e2a074cb74354b479baa6575
dhaferHS <dhafer.darkprod@gmail.com>
2022-09-28 16:59:44 +0100

    3-is_kind_of_class.py

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" module for instance objects"""
+
+
+def is_kind_of_class(obj, a_class):
+        """ return if object is instance is object or not """
+        return isinstance(obj, a_class)
