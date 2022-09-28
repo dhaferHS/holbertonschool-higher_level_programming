f4e2d20663377467ea823bc03a45aae426d6f707
dhaferHS <dhafer.darkprod@gmail.com>
2022-09-28 16:47:20 +0100

    2-is_same_class.py

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -2,4 +2,4 @@
 """ class for same class"""
 def is_same_class(obj, a_class):
     """check if the object is the same as the class"""
-    return type(obj == a_class)
+    return type(obj) == a_class
