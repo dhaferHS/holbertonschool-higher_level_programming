d0e0f07af3ffc4b0e6e5bdde7c7a97b02c77f5cb
dhaferHS <dhafer.darkprod@gmail.com>
2022-09-28 14:23:18 +0100

    0-lookup.py

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ function that returns the list of availble aatributes and methods of an object"""
 
+
 def lookup(obj):
     '''Lookup object attributes and methods.
     Args:
