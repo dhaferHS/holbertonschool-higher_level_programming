e5fb295dfb5c77d6d30a4594864f472ed8fc5d06
dhaferHS <dhafer.darkprod@gmail.com>
2022-09-29 11:31:08 +0100

    4-inherits_from.py

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ module for inherits """
+
+
 def inherits_from(obj, a_class):
     """return if the object in one of class that s inherted"""
-    return isinstance(obj, a_class) and type(obj) != a_class
+    return isinstance(obj, a_class) and not isinstance(obj, a_class)
