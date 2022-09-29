e1ed3d53f334cf69551f5e524d906e534206d9eb
dhaferHS <dhafer.darkprod@gmail.com>
2022-09-29 12:54:20 +0100

    6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""module for the empty class"""
+
+
+class BaseGeometry:
+    """bass geometry area """
+    
+    def area(self):
+        """ implementation"""
+        raise Exception("area() is not implemented")
+
