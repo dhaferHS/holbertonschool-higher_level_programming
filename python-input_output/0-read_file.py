7e12faac4fbde13df9ce6e8f8d0f843e12c99f09
dhaferHS <dhafer.darkprod@gmail.com>
2022-10-05 10:33:15 +0100

    0-read_file.py

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""read a text file """
+
+def read_file(filename=""):
+    """a function that reads a text file UTF8"""
+    
+    with open('filename', encoding="utf-8") as filename:
+        print(filename.read(), end=""):
