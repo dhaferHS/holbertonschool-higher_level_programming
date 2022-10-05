638398f567db02a1a92fdca2064aaf257dafaef7
dhaferHS <dhafer.darkprod@gmail.com>
2022-10-05 13:41:44 +0100

    2-append_write.py

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -6,4 +6,4 @@ def write_file(filename="", text=""):
     """a function that appends a string at the end of a text file (UTF8)"""
     
     with open(filename, "a", encoding="utf-8") as f:
-        return filename.write(text)
+        return f.write(text)
