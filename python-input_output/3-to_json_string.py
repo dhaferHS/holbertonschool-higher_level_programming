6cc36cd7cac4ee4bea5aa8ddf4395bf2ad5c720e
dhaferHS <dhafer.darkprod@gmail.com>
2022-10-05 16:28:07 +0100

    3-to_json_string.py

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -6,5 +6,5 @@ import json
 
 
 def to_json_string(my_obj):
-    """ a function that returns the JSON representation of an object (string)"""
+    """ a function that returns the JSON representation of an object string"""
     return json.dumps(my_obj)
