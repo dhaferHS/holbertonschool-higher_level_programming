7570d4dc48fe9545491b6f87c813e69cab56f0ea
dhaferHS <dhafer.darkprod@gmail.com>
2022-10-06 16:56:06 +0100

    base.py

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""base class"""
+
+
+_nb_objects = 0
+
+def __init__(self, id=None):
+    """id"""
+    
+    if id is not None:
+        self.id = id
+    else:
+        base.__nb_objects += 1
+        self.id = base.__nb_objects
