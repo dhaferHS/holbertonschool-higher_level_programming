00fa2dea3a646f8ca9dd4ec506f598935ebaadaa
dhaferHS <dhafer.darkprod@gmail.com>
2022-10-06 12:28:18 +0100

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,12 +2,12 @@
 """load add and save """
 
 
-
 import json
 
 
-save_to_json_file = __import__ ('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__(
+    '6-load_from_json_file.py').load_from_json_file
 
 filename = "add_item.json"
 
