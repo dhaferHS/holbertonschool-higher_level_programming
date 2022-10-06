a9759089a3cd65e2e9fe52d4bef2471734949ce8
dhaferHS <dhafer.darkprod@gmail.com>
2022-10-06 14:09:03 +0100

    10-student.py

---
+++ python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""student module"""
+
+
+class Student:
+    """student class """
+
+    def __init__(self, first_name, last_name, age):
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+
+    def to_json(self, attrs=None):
+        """a dictionary for student data"""
+        if attrs is None:
+            return self.__dict__
+        newdict = {}
+        for i in attrs:
+            try:
+                newdict[i] = self.__dict__[i]
+            except BaseException:
+                pass
+    
+        return newdict
