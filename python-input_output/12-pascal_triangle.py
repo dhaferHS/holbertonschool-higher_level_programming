5855093a83931ce1391649d2768400282a0720dc
dhaferHS <dhafer.darkprod@gmail.com>
2022-10-06 14:49:47 +0100

    12-pascal_triangle.py

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -6,7 +6,7 @@ def pascal_triangle(n):
     """ Pascal’s triangle of n"""
     if n <= 0:
         return []
-    
+
     triangle = [[1]]
     while len(triangle) != n:
         ntri = triangle[-1]
