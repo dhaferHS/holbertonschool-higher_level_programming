5febf4d024c9f0cc2fbaf78db13ff40dba379d22
dhaferHS <dhafer.darkprod@gmail.com>
2022-10-06 12:53:52 +0100

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
-"""
-load add and save
-"""
+"""Load add and save."""
 
 
 from sys import argv
 
-
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__(
-    '6-load_from_json_file.py').load_from_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 filename = "add_item.json"
 
 try:
-    adding = load_from_json_file(filename)
-
+    adding = load_from_json_file(filename)    
 except FileNotFoundError:
     adding = []
 
