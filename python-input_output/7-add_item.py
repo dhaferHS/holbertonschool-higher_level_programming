92573e4d95d9b0be27896fbace0d690d365b5e9e
dhaferHS <dhafer.darkprod@gmail.com>
2022-10-06 12:27:51 +0100

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""load,add and save """
+"""load add and save """
+
 
 
 import json
+
+
 save_to_json_file = __import__ ('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
 
