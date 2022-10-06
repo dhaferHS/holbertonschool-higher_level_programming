f162702ce04dc6a06b66afe48b521592fd4a1353
dhaferHS <dhafer.darkprod@gmail.com>
2022-10-06 16:57:43 +0100

    base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """base class"""
 
+class Base:
+    _nb_objects = 0
 
-_nb_objects = 0
-
-def __init__(self, id=None):
-    """id"""
+    def __init__(self, id=None):
+        """id"""
     
-    if id is not None:
-        self.id = id
-    else:
-        base.__nb_objects += 1
-        self.id = base.__nb_objects
+        if id is not None:
+            self.id = id
+        else:
+            base.__nb_objects += 1
+            self.id = base.__nb_objects
