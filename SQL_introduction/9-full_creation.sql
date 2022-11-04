f07209199ac9956af4c53e3c78ff26cba3120adb
dhaferHS <dhafer.darkprod@gmail.com>
2022-11-04 23:49:49 +0100

    9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -4,8 +4,5 @@ CREATE TABLE IF NOT EXISTS second_table
 	id INT,
 	name VARCHAR(256),
 	score INT
-)
-insert into second_table (id, name, score) VALUES(1, 'John', 10);
-insert into second_table (id, name, score) VALUES(2, 'Alex', 3);
-insert into second_table (id, name, score) VALUES(3, 'Bob', 14);
-insert into second_table (id, name, score) VALUES(4, 'George', 8);
+);
+INSERT INTO second_table VALUES(1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
