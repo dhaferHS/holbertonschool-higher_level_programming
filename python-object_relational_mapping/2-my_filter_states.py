b78a9fa05bc02ab372ab7d30d8bf36910fa34d0d
dhaferHS <dhafer.darkprod@gmail.com>
2022-11-15 00:38:55 +0100

    2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+ script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
+"""
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        password=argv[2],
+        database=argv[3],
+
+    )
+    thecursor = db.cursor()
+    thecursor.execute(
+        "SELECT id, name FROM states WHERE name LIKE BINARY '{}'ORDER BY id ASC"
+        .format(argv[4]))
+    rows = thecursor.fetchall()
+    for row in rows:
+        print(row)
+    thecursor.close()
+    db.close()
