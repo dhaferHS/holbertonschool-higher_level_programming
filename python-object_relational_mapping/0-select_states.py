4c412d0da8cf3d648330d8601367d3343a37879a
dhaferHS <dhafer.darkprod@gmail.com>
2022-11-14 20:12:36 +0100

    0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -10,13 +10,12 @@ if __name__ == "__main":
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        username=argv[1],
+        user=argv[1],
         password=argv[2],
         database=argv[3],
     )
 
     thecursor = db.cursor()
-
     thecursor.execute("SELECT id, name FROM states ORDED BY id ASC")
 
     therows = thecursor.fetchall()
