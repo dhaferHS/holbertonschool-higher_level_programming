0338d3de44622e344091c8874d2c0f93f38dab1d
dhaferHS <dhafer.darkprod@gmail.com>
2022-11-15 00:36:31 +0100

    1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-script that lists all states from the database hbtn_0e_0_usa
+ a script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
 """
 
 
