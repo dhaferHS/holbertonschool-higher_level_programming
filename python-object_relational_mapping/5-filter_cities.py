cfced77a87004d642d3d8a23a625d5d249d3be3d
dhaferHS <dhafer.darkprod@gmail.com>
2022-11-15 16:36:00 +0100

    5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -26,8 +26,7 @@ if __name__ == "__main__":
         INNER JOIN states
         ON cities.state_id = states.id
         WHERE states.name = %s
-        ORDER BY cities.id """
-        ,(argv[4], )
+        ORDER BY cities.id """, (argv[4], )
     )
     rows = thecursor.fetchall()
     if rows == ():
