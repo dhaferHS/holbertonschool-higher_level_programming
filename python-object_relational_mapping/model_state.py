d4d0d7e825782d5a71ebfd9dac78cd74f7e13cd6
dhaferHS <dhafer.darkprod@gmail.com>
2022-11-15 17:47:57 +0100

    model_state

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -8,9 +8,9 @@ Base = declarative_base()
 
 
 class State(Base):
-     """This class links to the `states` table of our database with id and name as attributes"""
-     
-     __tablename__ = 'states'
-    
-     id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
-     name =  Column(String(128), nullable=False)
+    """This class links to the `states` table of our database with id and name as attributes"""
+
+    __tablename__ = 'states'
+
+    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
