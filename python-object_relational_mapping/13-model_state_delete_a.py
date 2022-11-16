fe88a989323f425c6a0af1627bcd30e32fb3518e
dhaferHS <dhafer.darkprod@gmail.com>
2022-11-16 22:10:16 +0100

    13

---
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from sqlalchemy.orm import Session
+import model_state
+from model_state import Base, State
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            user, password, database),
+        pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+
+    for state in session.query(State).order_by(State.id.asc()):
+        if 'a' in state.name:
+            session.delete(state)
+        session.commit()
+    session.close()
