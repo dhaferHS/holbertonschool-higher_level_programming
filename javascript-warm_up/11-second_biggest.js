5cd68a789f48e5ab485c834b3682888ed778cb26
dhaferHS <dhafer.darkprod@gmail.com>
2022-12-29 17:39:47 +0100

    11-second_biggest.js

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+function nextBiggest(arr) {
+	let max = -Infinity, result = -Infinity;
+  
+	for (const value of arr) {
+	  const nr = Number(value)
+  
+	  if (nr > max) {
+		[result, max] = [max, nr]
+	  } else if (nr < max && nr > result) {
+		result = nr;
+	  }
+	}
+  
+	return result;
+  }
+  const arr = process.argv;
+  console.log(nextBiggest(arr));
