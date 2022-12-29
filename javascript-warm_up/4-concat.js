3f4c224f250d498e335664fe39b5b1e4e48367b7
dhaferHS <dhafer.darkprod@gmail.com>
2022-12-29 16:44:16 +0100

    4-concat.js

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,7 +1,2 @@
 #!/usr/bin/node
-const argv = process.argv;
-if (process.argv[2] === undefined) {
-  console.log('undefined is undefined');
-} else {
-  console.log(process.argv[2] + ' is ' + process.argv[3]);
-}
+console.log(process.argv[2] + ' is ' + process.argv[3]);
