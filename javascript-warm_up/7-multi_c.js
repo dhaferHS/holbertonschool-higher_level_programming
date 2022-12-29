922afdc5ca87f1bdc1b86065f853c661fadbf6f0
dhaferHS <dhafer.darkprod@gmail.com>
2022-12-29 17:18:28 +0100

    7-multi_c.js

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const x = parseInt(process.argv[2]);
+let i = 0;
+if (x) {
+  while (i < x) {
+    console.log('C is fun'); i++;
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
