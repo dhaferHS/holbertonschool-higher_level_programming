1b2d0e5363502808a18b2b2470f564eae9b4f010
dhaferHS <dhafer.darkprod@gmail.com>
2023-01-02 20:15:27 +0100

    6-square.js

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -7,10 +7,10 @@ module.exports = class Square extends Rectangle {
   }
 
   charPrint (c) {
-	if (c === undefined) {
-		c = 'X';
-	}
-	const size = this.height;
+    if (c === undefined) {
+      c = 'X';
+    }
+    const size = this.height;
     for (let i = 0; i < size; i++) {
       console.log(c.repeat(size));
     }
