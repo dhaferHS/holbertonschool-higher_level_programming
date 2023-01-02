f95a2cb8592e79fa0f0a71c89deb05f5e4c8a6f2
dhaferHS <dhafer.darkprod@gmail.com>
2023-01-02 11:26:42 +0100

    4-rectangle.js

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -2,27 +2,27 @@
 
 module.exports = class Rectangle {
   constructor (w, h) {
-	  if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-	  }
+	if (w > 0 && h > 0) {
+    this.width = w;
+    this.height = h;
+	}
   }
 
   print () {
-	  for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-	  }
-	  return 0;
+	for (let i = 0; i < this.height; i++) {
+    console.log('X'.repeat(this.width));
+	}
+	return 0;
   }
 
   double () {
-    this.height = 2 * this.height;
-    this.width = 2 * this.width;
+	this.height = 2 * this.height;
+	this.width = 2 * this.width;
   }
 
   rotate () {
-    const tmp = this.height;
-    this.height = this.width;
-    this.width = tmp;
+  const tmp = this.height;
+  this.height = this.width;
+  this.width = tmp;
   }
 };
