a8b7d403783b3bc9bfaedc18c5a147d170368db6
dhaferHS <dhafer.darkprod@gmail.com>
2023-01-08 16:40:03 +0100

    0-readme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,13 +1,14 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const {argv} = process;
+const { argv } = process;
 
 const path = argv[2];
 fs.readFile(path, 'utf8', (err, data) => {
-	if (err) {
-		console.error(err);
-		return;
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
 }
-    console.log(data);}
-	);
+);
