e321415b7321bbd603ac2c92b633157a85d2b300
dhaferHS <dhafer.darkprod@gmail.com>
2023-01-09 14:24:35 +0100

    3-starwars_title.js

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+
+const request = require('request');
+const apiurl = 'https://swapi-api.hbtn.io/api/films/';
+const argv = process.argv;
+
+request.get(apiurl, argv[2], function (error, response, body) {
+	if (error) {
+		console.log(error);
+	}
+	else {
+		console.log(JSON.parse(body).title);
+	}
+});
